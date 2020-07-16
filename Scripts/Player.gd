310d5fe4d6f67c4764f9678d6b03d7db9fcaa843
bonscji1 <ji.bonsch@seznam.cz>
2020-07-16 10:26:04 +0200

    Player movement/ placeholder player

---
+++ Scripts/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+const MAX_SPEED = 800
+const FRICTION = 400
+const ACCEL = 150
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector*ACCEL
+		velocity = velocity.clamped(MAX_SPEED)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+	
+	
+	move_and_slide(velocity)
